604eac2ffd199c2ce4ac9b45d07444a125732855
Jamel Lassoued | ジャメル <41418470+Sardonyx001@users.noreply.github.com>
2024-07-19 18:01:42 +0900

    feat(nvim): ✨ added lazygit plugin to nvim with custum keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- map("n", "<leader>gl", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
+map("n", "<leader>gl", function()
+  LazyVim.lazygit({ cwd = LazyVim.root.git() })
+end, { desc = "Lazygit " })
+map("n", "<leader>gL", function()
+  LazyVim.lazygit({ args = { "log" } })
+end, { desc = "Lazygit Log (cwd)" })

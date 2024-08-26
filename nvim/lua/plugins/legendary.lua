d86c77d6e69321acbed220aecf9583f8f2e7c38e
jam <41418470+Sardonyx001@users.noreply.github.com>
2024-08-27 00:01:10 +0900

    feat(nvim): add legendary.nvim

---
+++ nvim/lua/plugins/legendary.lua
@@ -0,0 +1,14 @@
+return {
+  "mrjones2014/legendary.nvim",
+  version = "v2.13.9",
+  -- since legendary.nvim handles all your keymaps/commands,
+  -- its recommended to load legendary.nvim before other plugins
+  priority = 10000,
+  lazy = false,
+  -- sqlite is only needed if you want to use frecency sorting
+  -- dependencies = { 'kkharji/sqlite.lua' }
+  keymaps = {
+    -- map keys to a command
+    { "<leader>L", ":Legendary", description = "Legendary.nvim" },
+  },
+}

9a3792de8d4d3254fc15f19c0345df34c2b01973
jam <41418470+Sardonyx001@users.noreply.github.com>
2024-08-27 00:01:42 +0900

    fix(nvim): update neo-tree config to hijack netrw

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,25 +1,46 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  lazy = false,
+  init = function()
+    vim.api.nvim_create_autocmd("BufEnter", {
+      -- make a group to be able to delete it later
+      group = vim.api.nvim_create_augroup("NeoTreeInit", { clear = true }),
+      callback = function()
+        local f = vim.fn.expand("%:p")
+        if vim.fn.isdirectory(f) ~= 0 then
+          vim.cmd("Neotree current dir=" .. f)
+          -- neo-tree is loaded now, delete the init autocmd
+          vim.api.nvim_clear_autocmds({ group = "NeoTreeInit" })
+        end
+      end,
+    })
+    -- keymaps
+  end,
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
-    "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   opts = {
+    window = {
+      auto_expand_width = false,
+      width = 25,
+    },
     filesystem = {
+      hijack_netrw_behavior = "open_current",
+      use_libuv_file_watcher = true,
+      follow_current_file = { enabled = true },
       filtered_items = {
         visible = true,
         show_hidden_count = true,
         hide_dotfiles = false,
         hide_gitignored = false,
         hide_by_name = {
-          -- '.git',
+          ".git",
           ".DS_Store",
-          -- 'thumbs.db',
+          "node_modules",
         },
-        never_show = {},
+        never_show = { ".DS_Store" },
       },
     },
   },

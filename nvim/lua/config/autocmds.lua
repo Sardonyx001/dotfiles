84a2cf4f78c321f286099a26a5dbc21f51416808
jam <41418470+Sardonyx001@users.noreply.github.com>
2025-03-28 13:06:33 +0900

    feat(nvim): add autocmd to turn off spelling on markdown files because its so f annoying

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,3 +2,11 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 --
+
+-- Disable spellchecking for markdown and text files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "text" },
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})

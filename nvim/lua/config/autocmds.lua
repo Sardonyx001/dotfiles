1a12ffd331c96517d9c11fb8b8d494e70fb2e1ae
jam <lassouedjamel99@gmail.com>
2025-07-18 22:57:42 +0900

    faet(nvim): add autocmd to enable table view for csv files

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -10,3 +10,16 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = false
   end,
 })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = vim.api.nvim_create_augroup("csvview_loading", {}),
+  desc = "Add key shortcuts for csv files",
+  pattern = { "csv", "tsv" },
+  callback = function(ev)
+    local function setup_csvview_keys(buf)
+      vim.keymap.set("n", "<leader>c,", require("csvview").toggle, { buffer = buf, desc = "Enable table view" })
+    end
+    setup_csvview_keys(ev.buf)
+    require("csvview").enable()
+  end,
+})
